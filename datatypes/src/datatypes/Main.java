cf372ddecc03be7fba63594e84cecaf9313702ea
EsraOzdogan <esraozdogaan@gmail.com>
2021-06-30 00:59:21 +0300

    datatypes

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 12;
+		number = 129;
+	
+		double number2 = 12.5;
+		
+		char character = 'a';
+		boolean isTrue = false;
+		
+	}
+
+}
