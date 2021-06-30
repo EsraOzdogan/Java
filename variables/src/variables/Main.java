bbaee88927268ec6ebc09e9be5d1efb73d9469a8
EsraOzdogan <esraozdogaan@gmail.com>
2021-06-30 19:13:52 +0300

    variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//camelCase
+		int studentNumber=10;
+		String message="Student number is";
+		System.out.println("Student number is: " + studentNumber);
+		System.out.println(message + " " +studentNumber);
+
+	}
+
+}
