428d1ad0c4599c13407d50784428d6ad53bf986a
EsraOzdogan <esraozdogaan@gmail.com>
2021-06-30 18:02:01 +0300

    interfaceDemo

---
+++ interfaceDemo/src/interfaceDemo/OutsourceWorker.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+//Disardan gelen calisan sadece calisma iþlevine sahiptir.
+public class OutsourceWorker implements IWorkable{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ interfaceDemo/src/interfaceDemo/Worker.java
@@ -0,0 +1,23 @@
+package interfaceDemo;
+///bizim calisanimiz calisma, yeme ve ödeme fonksiyonlarýna sahiptir
+public class Worker implements IWorkable, IEatable, IPayable{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pay() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
