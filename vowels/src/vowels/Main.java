6185cf1c41d6d0645c783db3c3f69a8204db66df
EsraOzdogan <esraozdogaan@gmail.com>
2021-06-30 19:14:32 +0300

    vowels

---
+++ vowels/src/vowels/Main.java
@@ -0,0 +1,23 @@
+package vowels;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char letter = 'E';
+		switch(letter) {
+		
+			case 'A' :
+			case 'I' :
+			case 'O' :
+			case 'U' :
+				System.out.println("bold vowels");
+				break;
+			default:
+				System.out.println("thin vowels");
+
+			
+		}
+
+	}
+
+}
