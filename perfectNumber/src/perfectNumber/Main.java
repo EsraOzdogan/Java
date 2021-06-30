94af8d17f699e85bd289d305840291d5c6612d4c
EsraOzdogan <esraozdogaan@gmail.com>
2021-06-30 18:32:44 +0300

    perfectNumber

---
+++ perfectNumber/src/perfectNumber/Main.java
@@ -0,0 +1,28 @@
+package perfectNumber;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//6--> 1,2,3
+		//28-->1,2,4,7,14
+
+		int number = 6;
+		int total = 0;
+		
+		for(int i=1;i<number;i++) {
+			if(number % i == 0){
+				total = total + i;
+			}
+		}
+		
+		if(total==number) {
+			System.out.println("Number is perfect number");
+		}else{
+			System.out.println("Number is not perfect number");
+		}
+			 
+	}
+
+}
+
+
