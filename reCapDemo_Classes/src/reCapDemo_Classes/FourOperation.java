611bd06a3ef1c196d997498bbddb9a63658c4150
EsraOzdogan <esraozdogaan@gmail.com>
2021-06-30 18:41:31 +0300

    reCapDemo_Classes

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/FourOperation.java
@@ -0,0 +1,21 @@
+package reCapDemo_Classes;
+
+public class FourOperation {
+
+	public int Addition(int number1, int number2) {
+		return number1+number2;
+	}
+	
+	public int extraction(int number1, int number2) {
+		return number1-number2;
+	}
+	
+	public int multiplication(int number1, int number2) {
+		return number1*number2;
+	}
+	
+	
+	public int division(int number1, int number2) {
+		return number1/number2;
+	}
+}
---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		FourOperation fourOperation=new FourOperation();
+		int result= fourOperation.Addition(5, 6);
+		System.out.println(result);
+	}
+
+}
