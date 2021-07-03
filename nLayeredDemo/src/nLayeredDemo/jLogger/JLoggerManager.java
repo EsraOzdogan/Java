d03979cdca4ea1c8f14891ee567e3e7a9892a47c
EsraOzdogan <esraozdogaan@gmail.com>
2021-07-03 18:31:14 +0300

    nLayeredDemo

--- nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
+++ nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -2,7 +2,7 @@ package nLayeredDemo.jLogger;
 //Baskasýnýn yazdýgý bir proje diye dusun
 public class JLoggerManager {
 	public void log(String message) {
-		System.out.println("Logged with J Logger");
+		System.out.println("Logged with J Logger: " + message);
 	}
 
 }
